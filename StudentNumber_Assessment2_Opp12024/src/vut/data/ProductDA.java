470e0991d4acc90c297cb160413b471409830965
Pfano03 <successpfano@gmail.com>
2024-05-11 14:42:06 +0200

    statement edit

--- StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
+++ StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
@@ -12,7 +12,7 @@ public class ProductDA {
     //MODIFY THESE DECLARATIONS IF THEY ARE INCORRECTLY DECLARED
 
      private static Connection con;
-    private static Statement stmt;
+    private  Statement stmt;
      private static ResultSet rs;
      PreparedStatement ps;
    private static ArrayList<ProductPD> arProduct = new ArrayList<>();
