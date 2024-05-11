322415b310ccdd35e797964af95be0bace0cb070
Kabelo <kabzamonyela03@gmail.com>
2024-05-11 23:34:20 +0200

    Added a method for initialising the database connection

--- StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
+++ StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
@@ -2,6 +2,7 @@ package vut.data;
 
 import java.sql.*;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -12,10 +13,25 @@ public class ProductDA {
     //MODIFY THESE DECLARATIONS IF THEY ARE INCORRECTLY DECLARED
 
      private static Connection con;
-    private  Statement stmt;
+     private  Statement stmt;
      private static ResultSet rs;
      private static PreparedStatement ps;
-   private static ArrayList<ProductPD> arProduct = new ArrayList<>();
+     private static ArrayList<ProductPD> arProduct = new ArrayList<>();
 
+     public static initialise(){
+         final String USERNAME = "root";
+         final String PASSWORD = "";
+         final String URL = "jdbc:mysql://localhost/productDB";
+         final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+         try{
+             Class.forName(DRIVER);
+             con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+         } catch (ClassNotFoundException ex) {
+             JOptionPane.showMessageDialog(null, "Class not found!");
+         } catch (SQLException ex){
+             JOptionPane.showMessageDialog(null, ex.getMessage());
+         }
+     }
     
 }
