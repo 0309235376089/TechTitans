29cc5b500c34880c4b85f92258a660d64b87cf25
MphoMutlaneng <mutlanengangel@gmail.com>
2024-05-12 01:32:45 +0200

    Prcatices

--- StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
+++ StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
@@ -46,5 +46,5 @@ public class ProductDA {
             System.out.println("Error replenishing product.\n" + e.getMessage());
         }
     }
-    
+    //Adding an unkwonk comment, just practicing to push
 }
