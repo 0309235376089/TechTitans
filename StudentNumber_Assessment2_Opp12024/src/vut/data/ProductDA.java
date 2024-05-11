b785fb5ba133cbc1945afbf5cbec5dce83d943b7
Andile <andilezulu200406@gmail.com>
2024-05-11 23:34:34 +0200

    AddProduct

--- StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
+++ StudentNumber_Assessment2_Opp12024/src/vut/data/ProductDA.java
@@ -17,5 +17,17 @@ public class ProductDA {
      private static PreparedStatement ps;
    private static ArrayList<ProductPD> arProduct = new ArrayList<>();
 
-    
+    public static void addProduct(ProductPD prodObj) {
+        String qry = "INSERT INTO tblProducts (prodName, price, quantity, prodCode, category) VALUES (?, ?, ?, ?, ?)";
+        try {
+            ps = con.prepareStatement(qry);
+            ps.setString(1, prodObj.getProdName());
+            ps.setDouble(2, prodObj.getPrice());
+            ps.setInt(3, prodObj.getQuantity());
+            ps.setInt(4, prodObj.getProdCode());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error adding product.\n" + e.getMessage());
+        }
+    }
 }
